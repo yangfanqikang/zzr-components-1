aaea1ec15057e0481c6fee34a5d84fc12b2128e4
Administrator <yangfanqikang@163.com>
2020-01-13 17:53:18 +0800

    1.添加input输入框组件,回去总结新吸收的知识以及组件的路数

--- docs/.vuepress/components/input-demos.vue
+++ docs/.vuepress/components/input-demos.vue
@@ -4,8 +4,31 @@
       <div>
         <h4>基础用法</h4>
         <div>
-          <zzr-input></zzr-input>
+          <div class="input-demos-ex1">
+            <zzr-input v-model="input1" placeholder="请输入内容"></zzr-input>
+          </div>
         </div>
+        <pre><code>{{content1}}</code></pre>
+      </div>
+      <div>
+        <h4>禁用状态</h4>
+        <p>通过 disabled 属性指定是否禁用 input 组件</p>
+        <div>
+          <div class="input-demos-ex1">
+            <zzr-input v-model="input2" placeholder="请输入内容" :disabled="true"></zzr-input>
+          </div>
+        </div>
+        <pre><code>{{content2}}</code></pre>
+      </div>
+      <div>
+        <h4>可清空</h4>
+        <p>使用clearable属性即可得到一个可清空的输入框</p>
+        <div>
+          <div class="input-demos-ex1">
+            <zzr-input v-model="input3" placeholder="请输入内容" clearable ></zzr-input>
+          </div>
+        </div>
+        <pre><code>{{content3}}</code></pre>
       </div>
     </div>
 </template>
@@ -14,12 +37,30 @@
   import ZzrInput from '../../../src/components/global/input/zzr-input'
   export default {
     name: 'input-demos',
-    components: { ZzrInput }
+    components: { ZzrInput },
+    data () {
+      return {
+        input1: 'input1',
+        input2: 'input1',
+        input3: '',
+        content1: `
+        <zzr-input v-model="input1" placeholder="请输入内容"></zzr-input>
+        `.replace(/^ {8}/gm, '').trim(),
+        content2: `
+        <zzr-input v-model="input2" placeholder="请输入内容" :disabled="true"></zzr-input>
+        `.replace(/^ {8}/gm, '').trim(),
+        content3: `
+        <zzr-input v-model="input3" placeholder="请输入内容" clearable></zzr-input>
+        `.replace(/^ {8}/gm, '').trim(),
+      }
+    },
   }
 </script>
 
 <style lang="scss" scoped>
     .input-demos {
-
+      .input-demos-ex1{
+        width: 180px;
+      }
     }
 </style>
--- src/components/global/input/zzr-input.vue
+++ src/components/global/input/zzr-input.vue
@@ -1,15 +1,141 @@
 <template>
-    <div class="zzr-input">zzr-input</div>
+    <div :class="[
+      type === 'textarea' ? 'zzr-textarea' : 'zzr-input',
+      inputSize ? 'zzr-input--' + inputSize : '',
+      {
+        'is-disabled': inputDisabled,
+        'zzr-input--suffix': $slots.suffix || suffixIcon || clearable
+      }
+    ]" @mouseenter="hovering = true" @mouseleave="hovering = false">
+      <template v-if="type !== 'textarea'">
+<!--        前置-->
+        <div class="zzr-input-group__prepend" v-if="$slots.prepend">
+          <slot name="prepend"></slot>
+        </div>
+        <input type="text"
+        v-if="type !== 'textarea'"
+        class="zzr-input__inner"
+               ref="input"
+               v-bind="$attrs"
+               @input="handleInput"
+               @focus="handleFocus"
+               @blur="handleBlur"
+               @change="handleChange"
+        >
+<!--        前置内容-->
+        <span class="zzr-input__prefix" v-if="this.$slots.prefix || prefixIcon">
+          <slot name="prefix"></slot>
+          <i class="zzr-input__icon"></i>
+        </span>
+<!--        后置内容-->
+        <span class="zzr-input__suffix" v-if="getSuffixVisible()">
+          <span class="zzr-input__suffix-inner">
+            <template v-if="!showClear">
+              <slot name="suffix"></slot>
+              <i class="zzr-input__icon"></i>
+            </template>
+            <span v-if="showClear" @click="clear">
+              <zzr-icon name="roundclose" class="zzr-icon__icon zzr-input__clear"></zzr-icon>
+            </span>
+
+          </span>
+        </span>
+<!--        后置元素-->
+        <div class="zzr-input-group__append" v-if="$slots.append">
+          <slot name="append"></slot>
+        </div>
+      </template>
+    </div>
 </template>
 
 <script>
+import ZzrIcon from '../icon/zzr-icon'
 export default {
-  name: 'zzr-input'
+  name: 'zzr-input',
+  components: { ZzrIcon },
+  props: {
+    type: {
+      type: String,
+      defalut: ''
+    },
+    value: [String, Number],
+    size: String,
+    disabled: Boolean,
+    clearable: {
+      type: Boolean,
+      default: true
+    },
+    suffixIcon: String,
+    prefixIcon: String
+  },
+  data () {
+    return {
+      hovering: false,
+      focused: false
+    }
+  },
+  computed: {
+    inputSize () {
+      return this.size
+    },
+    inputDisabled () {
+      return this.disabled
+    },
+    nativeInputValue () {
+      // 根据v-model的输入值判断值
+      return this.value === null || this.value === undefined ? '' : String(this.value)
+    },
+    showClear () {
+      // 当clearable存在, 获取焦点或者悬停时,showClear为true
+      return this.clearable && !this.inputDisabled && this.nativeInputValue && (this.focused || this.hovering)
+    }
+  },
+  watch: {
+    nativeInputValue () {
+      this.setNativeInputValue()
+    }
+  },
+  mounted () {
+    this.setNativeInputValue()
+  },
+  methods: {
+    handleFocus (event) {
+      this.focused = true
+    },
+    handleBlur (event) {
+      this.focused = false
+    },
+    handleInput (event) {
+      if (event.target.value === this.nativeInputValue) return
+      this.$emit('input', event.target.value)
+      this.$nextTick(this.setNativeInputValue)
+    },
+    handleChange (event) {
+
+    },
+    clear () {
+      // this.$emit('input', '')
+      console.log(111111111111)
+      this.$refs.input.value = ''
+      // this.$emit('change', '')
+    },
+    getSuffixVisible () {
+      return this.$slots.suffix || this.suffixIcon || this.showClear
+    },
+    getInput () {
+      return this.$refs.input || this.$refs.textarea
+    },
+    // 获取输入框的值,判断是否为空和是否与设置值一样
+    setNativeInputValue () {
+      const input = this.getInput()
+      if (!input) return
+      if (input.value === this.nativeInputValue) return
+      input.value = this.nativeInputValue
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-    .zzr-input {
-
-    }
+   @import "zzr-input";
 </style>
